c857a91d0165095b5e1059fa4db4e977664be093
Anatoliy Kurinnyy <one.quaker@gmail.com>
2019-06-15 13:18:21 +0400

    slug added

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+
+from .models import City, Category, Job, Employer
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('name', 'employer', 'slug', 'created_at', 'salary')
+    list_filter = ('employer', 'created_at', 'salary')
+
+
+admin.site.register(City)
+admin.site.register(Category)
+admin.site.register(Job, JobAdmin)
+admin.site.register(Employer)
--- src/web/models.py
+++ src/web/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField, ArrayField
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+
+from slugify import slugify
 
 
 class CreatedMixin(models.Model):
@@ -16,22 +20,26 @@ class PositionMixin(models.Model):
         abstract = True
 
 
-class City(models.Model):
-    name = models.CharField(max_length=64)
+class NameSlugMixin(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, blank=True, default='')
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
 
 
-class Category(CreatedMixin):
-    name = models.CharField(max_length=64)
-
-    def __str__(self):
-        return self.name
+class City(NameSlugMixin):
+    pass
 
 
-class Employer(CreatedMixin):
-    name = models.CharField(max_length=64)
+class Category(CreatedMixin, NameSlugMixin):
+    logo = models.ImageField(upload_to='category', blank=True, default='')
+
+
+class Employer(CreatedMixin, NameSlugMixin):
     legal_form = models.CharField(max_length=64)
     email = models.EmailField(max_length=256)
     phone = models.CharField(max_length=64)
@@ -39,11 +47,8 @@ class Employer(CreatedMixin):
     trusted = models.BooleanField(default=False)
     logo = models.ImageField(upload_to='employer', blank=True, default='')
 
-    def __str__(self):
-        return self.name
 
-
-class Job(CreatedMixin):
+class Job(CreatedMixin, NameSlugMixin):
     AGE_CHOICES = (
         (14, 14),
         (15, 15),
@@ -51,7 +56,6 @@ class Job(CreatedMixin):
         (17, 17),
     )
 
-    name = models.CharField(max_length=64)
     city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
     age = models.PositiveSmallIntegerField(choices=AGE_CHOICES)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
@@ -62,5 +66,22 @@ class Job(CreatedMixin):
     class Meta:
         ordering = ['created_at', ]
 
-    def __str__(self):
-        return self.name
+
+@receiver(pre_save, sender=City)
+def city_slug(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.name.lower())
+
+
+@receiver(pre_save, sender=Category)
+def category_slug(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.name.lower())
+
+
+@receiver(pre_save, sender=Employer)
+def employer_slug(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.name.lower())
+
+
+@receiver(pre_save, sender=Job)
+def job_slug(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.name.lower())
